a507ab611c7a498b9f20fac287a677dc0f2e9181
mook <rla0591@gmail.com>
2022-12-16 15:17:57 +0900

    refactoring arrangeMenuBtn v1

--- src/components/ArrangeMenuBtn.jsx
+++ src/components/ArrangeMenuBtn.jsx
@@ -3,14 +3,15 @@ import ArrangeMenu from './ArrangeMenu';
 
 const ArrangeMenuBtn = ({ results, setResults, isBusking }) => {
   const [isShowArrangeMenu, setIsShowArrangeMenu] = useState(false);
+  const onBtnClick = (e) => {
+    e.preventDefault();
+    setIsShowArrangeMenu(true);
+  };
   return (
     <div className='relative'>
       <button
         className='ml-5 bg-blue-600 max-lg:ml-2 max-lg:px-2 py-2 px-3 text-lg rounded-lg text-white hover:scale-125'
-        onClick={(e) => {
-          e.preventDefault();
-          setIsShowArrangeMenu(true);
-        }}
+        onClick={onBtnClick}
       >
         정렬
       </button>
