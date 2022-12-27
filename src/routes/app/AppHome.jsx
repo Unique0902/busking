eb93ba277d603b2e8571205fe4f3d42f1034a7af
mook <rla0591@gmail.com>
2022-12-27 14:29:08 +0900

    add redirectPage

--- src/routes/app/AppHome.jsx
+++ src/routes/app/AppHome.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import MainSec from '../../components/MainSec';
+import SongTable from '../../components/SongTable';
+import { usePlaylistContext } from '../../context/PlaylistContext';
 import { useUserDataContext } from '../../context/UserDataContext';
 
 export default function AppHome() {
   const { userData } = useUserDataContext();
+  const { nowPlaylist } = usePlaylistContext();
   return (
     <>
       <MainSec>
         <div className=''>
-          <h1 className='text-black text-3xl font-sans font-bold'>
+          <h1 className='text-black text-3xl font-sans font-bold mb-5'>
             어서오세요! {userData && userData.name} 님!
           </h1>
+          {nowPlaylist ? (
+            <h2 className='text-black text-3xl font-sans font-normal'>
+              현재 플레이리스트는 {nowPlaylist.name}입니다.
+            </h2>
+          ) : (
+            <h2 className='text-black text-3xl font-sans font-bold'>
+              현재 플레이리스트가 존재하지않습니다.
+            </h2>
+          )}
         </div>
       </MainSec>
     </>
